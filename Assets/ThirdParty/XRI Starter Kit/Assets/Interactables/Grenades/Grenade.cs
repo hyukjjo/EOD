978e7e83c395413633673afeaa6f05ad87ccc729
hyukjjo <48501630+hyukjjo@users.noreply.github.com>
2026-03-18 23:33:13 +0900

    feat: Unity Project Upload

---
+++ Assets/ThirdParty/XRI Starter Kit/Assets/Interactables/Grenades/Grenade.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+namespace MikeNspired.UnityXRHandPoser
+{
+    public class Grenade : MonoBehaviour, IDamageable
+    {
+        [SerializeField] private XRGrabInteractable interactable = null;
+        [SerializeField] private GameObject Explosion = null;
+        [SerializeField] private AudioSource activationSound = null;
+        [SerializeField] private GameObject meshLightActivation = null;
+        [SerializeField] private float detonationTime = 3;
+        //Proto
+        [SerializeField] protected bool startTimerAfterActivation = false;
+
+        //Proto
+        protected bool canActivate;
+        private XRInteractionManager interactionManager;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            OnValidate();
+            interactable = GetComponent<XRGrabInteractable>();
+            interactable.onActivate.AddListener(TurnOnGrenade);
+            interactable.onSelectExited.AddListener(Activate);
+            if (meshLightActivation)
+                meshLightActivation.SetActive(false);
+        }
+
+        private void OnValidate()
+        {
+            if (!interactable)
+                interactable = GetComponent<XRGrabInteractable>();
+            if (!interactionManager)
+                interactionManager = FindObjectOfType<XRInteractionManager>();
+        }
+        //Proto
+        protected virtual void TurnOnGrenade(XRBaseInteractor interactor)
+        {
+            canActivate = true;
+            meshLightActivation.SetActive(true);
+            activationSound.Play();
+
+            if (startTimerAfterActivation)
+                Invoke(nameof(TriggerGrenade), detonationTime);
+        }
+
+        //Proto
+        private void Activate(XRBaseInteractor interactor)
+        {
+            if (canActivate && !startTimerAfterActivation)
+                Invoke(nameof(TriggerGrenade), detonationTime);
+        }
+
+        //Proto
+        protected virtual void TriggerGrenade()
+        {
+            Explosion.SetActive(true);
+            Explosion.transform.parent = null;
+            Explosion.transform.localEulerAngles = Vector3.zero;
+
+            if (interactable.selectingInteractor)
+                interactionManager.SelectExit(interactable.selectingInteractor, interactable);
+
+            StartCoroutine(MoveAndDisableCollider());
+            //gameObject.SetActive(false);
+            // Destroy(gameObject,1);
+        }
+
+        private IEnumerator MoveAndDisableCollider()
+        {
+            //objectToMove.GetComponent<CollidersSetToTrigger>()?.SetAllToTrigger();
+
+            transform.position += Vector3.one * 9999;
+            yield return new WaitForSeconds(Time.fixedDeltaTime * 2);
+            //Lets physics respond to collider disappearing before disabling object physics update needs to run twice
+            Destroy(gameObject);
+        }
+
+        public void TakeDamage(float damage, GameObject damager)
+        {
+            //Proto
+            //TriggerGrenade();
+        }
+    }
+}
---
+++ Assets/ThirdParty/XRI Starter Kit/Assets/Scripts/Reusable/ObjectSpawner.cs
@@ -0,0 +1,79 @@
+﻿// Author MikeNspired. 
+
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+namespace MikeNspired.UnityXRHandPoser
+{
+    public class ObjectSpawner : MonoBehaviour
+    {
+        public bool isActive = true;
+        [SerializeField] private bool onlySpawnIfRoom = true;
+        [SerializeField] private GameObject Prefab = null;
+        [SerializeField] private Transform spawnPoint = null;
+        [SerializeField] private float spawnTimer = 5;
+
+        private string name = string.Empty;
+        public bool hitDetect;
+        private float currentTimer = 0;
+
+        private void Start()
+        {
+            name = Prefab.name;
+        }
+
+        private void FixedUpdate()
+        {
+            if (!isActive) return;
+
+            if (!onlySpawnIfRoom)
+            {
+                TickTimerAndSpawn();
+                return;
+            }
+
+            if (hitDetect)
+                currentTimer = 0;
+            else
+                TickTimerAndSpawn();
+        }
+
+        private void TickTimerAndSpawn()
+        {
+            currentTimer += Time.deltaTime;
+            if (!(currentTimer >= spawnTimer)) return;
+            Spawn();
+            currentTimer = 0;
+        }
+
+        private void Spawn()
+        {
+            GameObject _gameObject = Instantiate(Prefab, spawnPoint.position, spawnPoint.rotation);
+            _gameObject.transform.SetParent(transform);
+            _gameObject.name = name;
+        }
+
+        private void OnTriggerStay(Collider other)
+        {
+            XRGrabInteractable xRGrabInteractable = other.GetComponentInParent<XRGrabInteractable>();
+
+            if(xRGrabInteractable)
+            {
+                if (other.GetComponentInParent<XRGrabInteractable>().name == name)
+                    hitDetect = true;
+            }
+        }
+
+
+        private void OnTriggerExit(Collider other)
+        {
+            XRGrabInteractable xRGrabInteractable = other.GetComponentInParent<XRGrabInteractable>();
+
+            if (xRGrabInteractable)
+            {
+                if (other.GetComponentInParent<XRGrabInteractable>().name == name)
+                    hitDetect = false;
+            }
+        }
+    }
+}
